2ea4975cbb3442a01ca2b3e1cf9911656a707405
TehWardy <paul.ward@ccoder.co.uk>
2022-11-21 16:43:07 +0000

    Tidy up for Nuget Setup

---
+++ src/Apps/SecurityMSSQL/Program.cs
@@ -0,0 +1,91 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Security.Data.EF.MSSQL;
+using Security.Data.EF;
+using Security.UserManager;
+using System;
+using System.IO;
+using System.Configuration;
+
+namespace Security
+{
+    public class Program
+    {
+        static string LogXML => @"<?xml version=""1.0"" encoding=""utf-8""?>
+<log4net>
+  <appender name=""rollingLogFile"" type=""log4net.Appender.RollingFileAppender"">
+    <file value=""FILEPATH"" />
+    <appendToFile value=""true"" />
+    <rollingStyle value=""Size"" />
+    <maxSizeRollBackups value=""FILECOUNT"" />
+    <maximumFileSize value=""FILESIZE"" />
+    <staticLogFileName value=""true"" />
+    <layout type=""log4net.Layout.PatternLayout"">
+      <conversionPattern value=""CONVERSIONPATTERN"" />
+    </layout>
+  </appender>
+  <root name=""APPNAME"">
+    <level value=""LEVEL"" />
+    <appender-ref ref=""rollingLogFile"" />
+  </root>
+</log4net>
+        ";
+
+        public static string GetLogXMLConfig(string appName, IConfiguration config) => LogXML
+            .Replace("APPNAME", appName)
+            .Replace("FILEPATH", config.GetSection("Logging")["FilePath"])
+            .Replace("FILECOUNT", config.GetSection("Logging")["FileCount"])
+            .Replace("FILESIZE", config.GetSection("Logging")["FileSize"])
+            .Replace("LEVEL", config.GetSection("Logging")["Level"])
+            .Replace("CONVERSIONPATTERN", config.GetSection("Logging")["ConversionPattern"]);
+
+        public static void Main(string[] args)
+        {
+            var configRoot = new ConfigurationBuilder()
+                .AddEnvironmentVariables(prefix: "ENV_")
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // configure DI for stack
+            builder.Services.AddAspNetCore();
+            builder.Services.AddMetadata();
+
+            builder.Services.AddSecurity(configRoot, services => {
+
+                services.AddTransient<ISecurityModelBuildProvider, SecurityMSSQLModelBuildProvider>();
+
+                services.AddDistributedSqlServerCache(options => {
+                    options.ConnectionString = configRoot.GetConnectionString("SSO");
+                    options.SchemaName = "dbo";
+                    options.TableName = "Sessions";
+                });
+            });
+
+            builder.Services.AddHsts(options =>
+            {
+                options.Preload = true;
+                options.IncludeSubDomains = true;
+                options.MaxAge = TimeSpan.FromMinutes(60);
+            });
+
+            builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
+
+            builder.Logging.ClearProviders();
+            builder.Logging.AddSimpleConsole();
+
+            var tempPath = Path.GetTempFileName();
+            File.WriteAllText(tempPath, GetLogXMLConfig("Web", builder.Configuration));
+            builder.Logging.AddLog4Net(tempPath);
+
+            var app = builder.Build();
+            app.UseSession();
+            app.UseTheFramework();
+            app.Run();
+        }
+    }
+}
---
+++ src/Apps/SecuritySQLite/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Security.Objects.DTOs;
+using Security.Services.Services.Orchestration.Interfaces;
+using System.Threading.Tasks;
+
+namespace Security.Controllers
+{
+    [Route("/Api/Account")]
+    public class AccountController : Controller
+    {
+        readonly ISSOUserAuthenticationOrchestrationService userService;
+
+        public AccountController(ISSOUserAuthenticationOrchestrationService userService)
+            => this.userService = userService;
+
+        public class Auth
+        { 
+            public string User { get; set; }
+            public string Pass { get; set; }
+        }
+
+        public class ChangePasswordRequest
+        {
+            public string OldPassword { get; set; }
+            public string NewPassword { get; set; }
+        }
+
+        [HttpPost("Login")]
+        public async ValueTask<IActionResult> Login([FromBody] Auth auth)
+            => ModelState.IsValid
+                ? Ok(await userService.Login(auth.User, auth.Pass))
+                : BadRequest(ModelState);
+
+        [HttpPost("Logout")]
+        public async ValueTask<IActionResult> Logout()
+        {
+            await userService.Logout();
+            return Ok();
+        }
+
+        [HttpPost("Register")]
+        public async ValueTask<IActionResult> Register([FromBody] RegisterUser registerForm)
+            => (ModelState.IsValid)
+                ? Ok(await userService.Register(registerForm))
+                : BadRequest(ModelState);
+
+        [HttpPost("ChangePassword")]
+        public async ValueTask<IActionResult> ChangePassword(ChangePasswordRequest request)
+        {
+            if (ModelState.IsValid)
+            {
+                await userService.ChangePassword(request.OldPassword, request.NewPassword);
+                return Ok();
+            }
+            else 
+                return BadRequest(ModelState);
+        }
+    }
+}
