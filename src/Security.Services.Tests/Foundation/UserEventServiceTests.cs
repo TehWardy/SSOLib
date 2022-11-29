9d1b8202983a932a02670ebc504f445536dc6d02
Callum Marshall <callum.marshall@corporatelinx.com>
2022-11-29 12:13:33 +0000

    AddUserEventAsyncWorksAsExpected -> FAIL

---
+++ src/Security.Services.Tests/Foundation/UserEventServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using Moq;
+using Security.Data.Brokers.DateTime;
+using Security.Data.Brokers.Storage.Interfaces;
+using Security.Objects.Entities;
+using Security.Services.Services.Foundation;
+using Security.Services.Services.Foundation.Interfaces;
+using System;
+using Tynamix.ObjectFiller;
+
+namespace Security.Services.Tests.Foundation
+{
+    public partial class UserEventServiceTests
+    {
+        private readonly Mock<ISecurityDateTimeOffsetBroker> dateTimeOffsetBrokerMock;
+        private readonly Mock<IUserEventBroker> userEventBrokerMock;
+        private readonly IUserEventService userEventService;
+
+        public UserEventServiceTests()
+        {
+            dateTimeOffsetBrokerMock = new Mock<ISecurityDateTimeOffsetBroker>();
+            userEventBrokerMock = new Mock<IUserEventBroker>();
+            userEventService = new UserEventService(userEventBrokerMock.Object, dateTimeOffsetBrokerMock.Object);
+        }
+
+        UserEvent RandomUserEvent()
+            => GetUserEventFiller().Create();
+
+        Filler<UserEvent> GetUserEventFiller()
+        {
+            var filler = new Filler<UserEvent>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(DateTimeOffset.Now)
+                .OnProperty(ue => ue.Session).IgnoreIt()
+                .OnProperty(ue => ue.CreatedByUser).IgnoreIt()
+                .OnProperty(ue => ue.Tenant).IgnoreIt();
+
+            return filler;
+        }
+    }
+}
---
+++ src/Security.Services/Services/Foundation/UserEventService.cs
@@ -0,0 +1,42 @@
+﻿using Security.Data.Brokers.DateTime;
+using Security.Data.Brokers.Storage.Interfaces;
+using Security.Objects.Entities;
+using Security.Services.Services.Foundation.Interfaces;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Security.Services.Services.Foundation
+{
+    public class UserEventService : IUserEventService
+    {
+        private readonly IUserEventBroker broker;
+        private readonly ISecurityDateTimeOffsetBroker dateTimeOffsetBroker;
+
+        public UserEventService(IUserEventBroker broker, ISecurityDateTimeOffsetBroker dateTimeOffsetBroker)
+        {
+            this.broker = broker;
+            this.dateTimeOffsetBroker = dateTimeOffsetBroker;
+        }
+
+        public ValueTask<UserEvent> AddUserEventAsync(UserEvent userEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask DeleteUserEventAsync(UserEvent userEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<UserEvent> GetAllUserEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<UserEvent> UpdateUserEventAsync(UserEvent userEvent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
