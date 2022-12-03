90a6c2c1ea2a575e043bceceb6c82b8b47ca404c
Callum Marshall <callum.marshall@corporatelinx.com>
2022-12-03 21:13:21 +0000

    PROCESSING: Implement TenantProcessingService

---
+++ src/Security.Services/Services/Processing/Interfaces/ITenantProcessingService.cs
@@ -0,0 +1,17 @@
+﻿using Security.Objects.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Security.Services.Services.Processing.Interfaces
+{
+    public interface ITenantProcessingService
+    {
+        ValueTask<Tenant> AddTenantAsync(Tenant item);
+        ValueTask<Tenant> UpdateTenantAsync(Tenant item);
+        ValueTask DeleteTenantAsync(Tenant item);
+        IQueryable<Tenant> GetAllTenants();
+    }
+}
---
+++ src/Security.Services/Services/Processing/TenantProcessingService.cs.cs
@@ -0,0 +1,30 @@
+﻿using Security.Objects.Entities;
+using Security.Services.Services.Foundation.Interfaces;
+using Security.Services.Services.Processing.Interfaces;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Security.Services.Services.Processing
+{
+    public class TenantProcessingService : ITenantProcessingService
+    {
+        private readonly ITenantService tenantService;
+
+        public TenantProcessingService(ITenantService tenantService)
+        {
+            this.tenantService = tenantService;
+        }
+
+        public ValueTask<Tenant> AddTenantAsync(Tenant item)
+            => tenantService.AddTenantAsync(item);
+
+        public ValueTask DeleteTenantAsync(Tenant item)
+            => tenantService.DeleteTenantAsync(item);
+
+        public IQueryable<Tenant> GetAllTenants()
+            => tenantService.GetAllTenants();
+
+        public ValueTask<Tenant> UpdateTenantAsync(Tenant item)
+            => tenantService.UpdateTenantAsync(item);
+    }
+}
