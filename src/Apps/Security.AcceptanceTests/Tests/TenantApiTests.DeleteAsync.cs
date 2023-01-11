33f10358881a68aec2b367fc4d7858a5092e1d55
Shannon Watson <shannon.watson@corporatelinx.co.uk>
2023-01-11 16:04:55 +0000

    ShouldDeleteTenants -> PASS

---
+++ src/Apps/Security.AcceptanceTests/Tests/TenantApiTests.DeleteAsync.cs
@@ -0,0 +1,22 @@
+﻿using Security.Objects.Entities;
+using System;
+using Xunit;
+
+namespace Security.AcceptanceTests.Tests
+{
+    public partial class TenantApiTests
+    {
+        [Fact]
+        public async void ShouldDeleteTenants()
+        {
+            // given
+            Tenant inputTenant = await client.AddTenantAsync(RandomTenant());
+
+            // when 
+            await client.DeleteTenantAsync(inputTenant.Id);
+
+            // then
+            await Assert.ThrowsAsync<Exception>(async () => await client.GetTenantById(inputTenant.Id));
+        }
+    }
+}
