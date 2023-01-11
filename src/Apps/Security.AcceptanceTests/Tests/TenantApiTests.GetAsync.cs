bc50a3959ec395c0cf268fd1df805f34f8a5a0b9
Shannon Watson <shannon.watson@corporatelinx.co.uk>
2023-01-11 16:14:34 +0000

    ShouldGetAllTenants -> PASS

--- src/Apps/Security.AcceptanceTests/Tests/TenantApiTests.GetAsync.cs
+++ src/Apps/Security.AcceptanceTests/Tests/TenantApiTests.GetAsync.cs
@@ -1,5 +1,10 @@
 ﻿using FluentAssertions;
+using Force.DeepCloner;
 using Security.Objects.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Security.AcceptanceTests.Tests
@@ -19,6 +24,41 @@ namespace Security.AcceptanceTests.Tests
             // then
             actualTenant.Should().BeEquivalentTo(expectedTenant);
             await client.DeleteTenantAsync(actualTenant.Id);
+        }
+
+        [Fact]
+        public async void ShouldGetAllTenants()
+        {
+            var randomSetSize = GetRandomNumber();
+
+            // given
+            var inputTenants = new List<Tenant>();
+            var expectedTenants = new List<Tenant>();
+
+            for (int i = 0; i < randomSetSize; i++)
+            {
+                var tenant = RandomTenant();
+                inputTenants.Add(tenant);
+                var newTenant = await client.AddTenantAsync(tenant);
+                var copy = tenant.DeepClone();
+                copy.Id = newTenant.Id;
+                copy.CreatedBy = "TestUser";
+                copy.LastUpdatedBy = "TestUser";
+                copy.CreatedOn = newTenant.CreatedOn;
+                copy.LastUpdated = newTenant.LastUpdated;
+                expectedTenants.Add(copy);
+            }
+
+            // when
+            var actualTenants = await client.GetTenantsAsync();
+
+            // then
+            foreach (var expectedTenant in expectedTenants)
+            {
+                var actualTenant = actualTenants.First(Tenant => Tenant.Id == expectedTenant.Id);
+                actualTenant.Should().BeEquivalentTo(expectedTenant);
+                await client.DeleteTenantAsync(actualTenant.Id);
+            }
         }
     }
 }
