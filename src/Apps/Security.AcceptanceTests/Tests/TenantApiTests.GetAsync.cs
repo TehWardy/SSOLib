ba2d34a4a2bf3f98b1246cdb8070e9976869f9be
Shannon Watson <shannon.watson@corporatelinx.co.uk>
2023-01-11 16:05:38 +0000

    ShouldGetTenantsById -> PASS

---
+++ src/Apps/Security.AcceptanceTests/Tests/TenantApiTests.GetAsync.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Security.Objects.Entities;
+using Xunit;
+
+namespace Security.AcceptanceTests.Tests
+{
+    public partial class TenantApiTests
+    {
+        [Fact]
+        public async void ShouldGetTenantsById()
+        {
+            // given
+            Tenant inputTenant = RandomTenant();
+            Tenant expectedTenant = await client.AddTenantAsync(inputTenant);
+
+            // when
+            var actualTenant = await client.GetTenantById(expectedTenant.Id);
+
+            // then
+            actualTenant.Should().BeEquivalentTo(expectedTenant);
+            await client.DeleteTenantAsync(actualTenant.Id);
+        }
+    }
+}
